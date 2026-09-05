8d728129e63964e83247a727fe5f57cc4a147f72
Hypnotic <hypnoticfbtclub@gmail.com>
2026-09-04 19:58:20 -0600

    HYPN V1.4.1: limpiar cargador antiguo de galeria

--- Web/config.js
+++ Web/config.js
@@ -1,10 +1,3 @@
 window.HYPN_CONFIG = {
   authWorkerUrl: "https://hypn-remote-image-auth.hypnoticfbtclub.workers.dev"
 };
-
-window.addEventListener('DOMContentLoaded', () => {
-  const script = document.createElement('script');
-  script.src = 'gallery-v14.js?v=140';
-  script.defer = true;
-  document.body.appendChild(script);
-});
